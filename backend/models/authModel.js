a20ca1055cb6498dfb43b195764be82305f92c75
Ula Jeziorna <jeziornaula@wp.pl>
2023-07-04 17:23:11 +0200

    User register cd creating a model RegisterSchema

---
+++ backend/models/authModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+const  RegisterSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+
+
+module.exports = mongoose.model('user', RegisterSchema)
